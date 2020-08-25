970962b52bc056edc6cd4b5080d96164b6fb16b4
inzynier domu <inzynier.domu@gmail.com>
2020-08-25 12:34:33 +0200

    add test json

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include "HAL/Keyboard.h"
+#include "test_json.h"
 
 #define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 64 
---
+++ src/test_json.h
@@ -0,0 +1,5 @@
+
+struct test_json
+{
+    const String content = "{\"SSID\":\"test_ssid\",\"PASS\":\"test_pass\"}";
+};
