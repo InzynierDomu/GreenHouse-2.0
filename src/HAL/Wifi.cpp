3fef0d53e36e11dddcd85ba9e987e7623c23f956
Inżynier Domu <inzynier.domu@gmail.com>
2022-05-30 12:37:49 +0200

    fix ip print

--- src/HAL/Wifi.cpp
+++ src/HAL/Wifi.cpp
@@ -63,7 +63,8 @@ void Wifi::connect_wifi(const char* ssid, const char* pass)
   }
 
   m_logger.log("Connected");
-  m_logger.log("IP address:" + WiFi.localIP());
+  auto address = WiFi.localIP();
+  m_logger.log("IP address: " + String(address[3]) + ":" + String(address[2]) + ":" + String(address[1]) + ":" + String(address[0]));
 }
 
 } // namespace HAL
