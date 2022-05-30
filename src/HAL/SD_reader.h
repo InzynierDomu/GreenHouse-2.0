d150bd9dca0e9c5fc35e536a00fc05d660504b52
Inżynier Domu <inzynier.domu@gmail.com>
2022-05-30 10:27:23 +0200

    change include sd lib

--- src/HAL/SD_reader.h
+++ src/HAL/SD_reader.h
@@ -1,15 +1,11 @@
 #pragma once
 
-#ifndef UNIT_TEST
-
-#include "SD.h"
-#include <CRC32.h>
-#include <SPI.h>
-
-#endif
-
 #include "Logger.h"
 #include "Pin_config.h"
+#include "SD.h"
+
+#include <CRC32.h>
+#include <SPI.h>
 
 namespace HAL
 {
@@ -19,7 +15,7 @@ class SD_reader
   public:
   SD_reader();
   bool is_card_available() const;
-  String get_json_file(); 
+  String get_json_file();
 
   private:
   Logger m_logger;
