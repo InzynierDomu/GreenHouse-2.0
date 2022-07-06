6187a84fe296589ccbda793d948f5b97837e7bbc
Inżynier Domu <inzynier.domu@gmail.com>
2022-07-06 11:32:01 +0200

    doxygen

--- src/HAL/Dht_sensor.cpp
+++ src/HAL/Dht_sensor.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file Dht_sensor.cpp
+ * @author by Szymon Markiewicz (https://github.com/InzynierDomu/)
+ * @brief air temperature and humidity sensor
+ * @date 07-2022
+ */
 #include "Dht_sensor.h"
 
 #include "Pin_config.h"
@@ -6,6 +12,9 @@
 namespace HAL
 {
 
+/**
+ * @brief Construct a new Dht_sensor::Dht_sensor object
+ */
 Dht_sensor::Dht_sensor()
 : m_logger(Logger("DHT_sensor", Real_clock::get_instance()->get_time_callback()))
 , m_sensor(DHT(Pins::dht_pin, DHT22))
@@ -13,11 +22,19 @@ Dht_sensor::Dht_sensor()
   m_sensor.begin();
 }
 
+/**
+ * @brief get temperature from sensor
+ * @return air temperature
+ */
 float Dht_sensor::get_temp()
 {
   return m_sensor.readTemperature();
 }
 
+/**
+ * @brief get humidity from sensor
+ * @return air humidity
+ */
 float Dht_sensor::get_hum()
 {
   return m_sensor.readHumidity();
